dcf0e15c44a259b95296bf7cc6f06e6f9b570261
q296009578 <296009578@qq.com>
2019-10-06 22:11:12 +0800

    first commit

---
+++ scripts/test_display.py
@@ -0,0 +1,13 @@
+
+class Test_abc:
+    def test_aaa(self):
+        print(111)
+        assert 1
+
+    def test_bbb(self):
+        print(222)
+        assert 1
+
+    def test_ccc(self):
+        print(333)
+        assert 0
